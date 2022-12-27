42e9a3e709410e898dc8e0ce3ef6956bae4183be
Nice <nicenicegame@gmail.com>
2022-12-27 17:26:20 +0700

    add socket.io

--- index.ts
+++ index.ts
@@ -1,15 +1,19 @@
 import express, { Express, Request, Response } from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
 const app: Express = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer)
 const port = process.env.PORT || 8000
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript server')
 })
 
-app.listen(port, () => {
+httpServer.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
