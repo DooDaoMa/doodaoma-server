36a454b1771841e855b30c7d276176afaaa04f02
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2022-12-27 21:25:55 +0700

    add auth router

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, Request, Response, NextFunction } from 'express'
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import dotenv from 'dotenv'
+import bodyParser from 'body-parser';
+import { authRouter } from './src/routes';
+import { requireJWTAuth } from './src/config/jwt.config';
 
 dotenv.config()
 
@@ -9,6 +12,14 @@ const app: Express = express()
 const httpServer = createServer(app)
 const io = new Server(httpServer)
 const port = process.env.PORT || 8000
+app.use(bodyParser.json());
+
+// register router
+app.use(authRouter);
+
+app.get('/home', requireJWTAuth, (req, res) => {
+  res.send("success");
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript server')
