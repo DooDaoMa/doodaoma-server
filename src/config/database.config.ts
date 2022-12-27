890288a744a8b6c32ba9c28ac4d42b2401d61bd9
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2022-12-27 21:25:46 +0700

    Mongoose init

---
+++ src/config/database.config.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
+// Set up default mongoose connection
+const mongoDB = "mongodb://localhost:27017/doodaoma";
+mongoose.connect(mongoDB, {
+});
+
+// Get the default connection
+export const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
---
+++ src/models/user.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { db } from "../config/database.config";
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+// Compile model from schema
+export const User = db.model("User", new Schema({
+  username: String,
+  password: String,
+  email: String,
+}));
