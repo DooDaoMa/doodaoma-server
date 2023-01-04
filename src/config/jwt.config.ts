ec450780e1879dd2953ab55731afb6c576476fa8
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-01-04 17:19:08 +0700

    change jwt lib

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,9 +1,7 @@
 import passport from 'passport'
 import { User } from '../models'
 import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt'
-import dotenv from 'dotenv'
-
-dotenv.config()
+import { SECRET_KEY } from './constant.config'
 
 export type User = {
   id: number
@@ -11,21 +9,18 @@ export type User = {
 }
 export type JwtPayload = User & {
   iat: number
-  sub: string
+  data: string
 }
 
 // authentication Strategy
 const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  secretOrKey: process.env.SECRET_KEY,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: SECRET_KEY,
 }
-export const jwtAuth = new JwtStrategy(
-  jwtOptions,
-  async (payload: JwtPayload, done: (arg0: null, arg1: boolean) => void) => {
-    const user = await User.findOne({ username: payload.sub })
-    if (user) done(null, true)
-    else done(null, false)
-  },
-)
+export const jwtAuth = new JwtStrategy(jwtOptions, async (payload, done) => {
+  const user = await User.findOne({ username: payload.data })
+  if (user) done(null, true)
+  else done(null, false)
+})
 passport.use(jwtAuth)
 export const requireJWTAuth = passport.authenticate('jwt', { session: false })
--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -1,34 +1,41 @@
 import express, { Request, Response } from 'express'
-import { loginMiddleware } from '../middlewares'
-import jwt from 'jwt-simple'
+import { loginMiddleware, checkDuplicateUsernameOrEmail } from '../middlewares'
+import jwt from 'jsonwebtoken'
 import { User } from '../models'
+import { SECRET_KEY } from '../config/constant.config'
 
 export const authRouter = express.Router()
 
 authRouter.post('/login', loginMiddleware, async (req, res) => {
   try {
-    const payload = {
-      sub: req.body.username,
-      iat: new Date().getTime(), //มาจากคำว่า issued at time (สร้างเมื่อ)
+    if (req.user) {
+      res.status(201).send(
+        jwt.sign({ data: req.body.username }, SECRET_KEY, {
+          expiresIn: '1h',
+        }),
+      )
     }
-    const SECRET = 'MY_SECRET_KEY' //ในการใช้งานจริง คีย์นี้ให้เก็บเป็นความลับ
-    res.status(201).send(jwt.encode(payload, SECRET))
   } catch {
     res.status(500)
   }
 })
 
-authRouter.post('/signup', async (req: Request, res: Response) => {
-  const payload = req.body
-  try {
-    const newUser = new User({
-      username: payload.username,
-      password: payload.password,
-      email: payload.email,
-    })
-    await newUser.save()
-    res.status(201).end()
-  } catch (error) {
-    res.status(400).json(error)
-  }
-})
+authRouter.post(
+  '/signup',
+  checkDuplicateUsernameOrEmail,
+  async (req: Request, res: Response) => {
+    const payload = req.body
+    try {
+      const newUser = new User({
+        username: payload.username,
+        password: payload.password,
+        email: payload.email,
+      })
+      newUser.setPassword(payload.password)
+      await newUser.save()
+      res.status(201).end()
+    } catch (error) {
+      res.status(400).json(error)
+    }
+  },
+)
