2c06d7519af1433dbb379670f05aee0d891bf80c
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-01-26 10:26:50 +0700

    accounts pagination

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import {
+  middleware as paginateMiddleware,
+  getArrayPages,
+} from 'express-paginate'
 import { requireJWTAuth } from '../config/jwt.config'
 import { User } from '../models/user'
 import { IUser } from 'types/user.types'
 
 export const accountRouter = Router()
+
+accountRouter.use(paginateMiddleware(10, 50))
 
 accountRouter.get('/account', requireJWTAuth, (req, res) => {
   const user = req.user as IUser
@@ -15,14 +21,29 @@ accountRouter.get('/account', requireJWTAuth, (req, res) => {
   }
 })
 
-accountRouter.get('/accounts', requireJWTAuth, async (req, res) => {
-  const userList = await User.find({})
-  if (userList) {
-    res.status(200).send({
-      data: userList.map((user: IUser) => ({
-        username: user.username,
-        email: user.email,
-      })),
-    })
+accountRouter.get('/accounts', requireJWTAuth, async (req, res, next) => {
+  const limit = +(req.query.limit || 10)
+  const skip = +(req.skip || 0)
+  const page = +(req.query.page || 1)
+  try {
+    const [results, itemCount] = await Promise.all([
+      User.find({}).limit(limit).skip(skip).lean().exec(),
+      User.count({}),
+    ])
+
+    const pageCount = Math.ceil(itemCount / limit)
+    if (results) {
+      res.status(200).send({
+        data: results.map((user: IUser) => ({
+          username: user.username,
+          email: user.email,
+        })),
+        pageCount,
+        itemCount,
+        pages: getArrayPages(req)(3, pageCount, page),
+      })
+    }
+  } catch (err) {
+    next(err)
   }
 })
