5596ac6f36232aa5ede5ca4420b39e9b5fcda230
Nice <nicenicegame@gmail.com>
2023-01-30 23:05:15 +0700

    add devices route

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import express, { json } from 'express'
 import { Server } from 'socket.io'
 import cors from 'cors'
 import { authRouter } from './routes/authRoute'
+import { devicesRouter } from './routes/devicesRoute'
 import { requireJWTAuth } from './config/jwt.config'
 import handler from './socket/handler'
 import { PORT } from './config/constant.config'
 
 const app = express()
 const httpServer = createServer(app)
+
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
 const io = new Server(httpServer)
 handler.registerSocketHandler(io)
 
@@ -17,6 +23,7 @@ app.use(json())
 
 // register router
 app.use(authRouter)
+app.use(devicesRouter)
 
 app.get('/home', requireJWTAuth, (req, res) => {
   res.json('success')
@@ -24,8 +31,4 @@ app.get('/home', requireJWTAuth, (req, res) => {
 
 app.get('/', (req, res) => {
   res.json('Express + TypeScript server')
-})
-
-httpServer.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
 })
