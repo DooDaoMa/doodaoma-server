aaa5e6b089a31817762779eece28b5725544d294
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-02-04 14:52:17 +0700

    change query reservation logic

--- src/middlewares/reservation.middleware.ts
+++ src/middlewares/reservation.middleware.ts
@@ -1,5 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
 import { Reservation } from '../models/reservation'
+import { getISOString } from '../utils/datetime'
+// import { isBefore, parseISO, format, isAfter } from 'date-fns'
+// import { generateDateTimeSlots, setTime } from '../utils/timeSlot'
+
+// const date = new Date(new Date())
+// date.setDate(date.getDate() + 2)
+// const timeSlot = generateDateTimeSlots(date)
 
 export const checkReservationPeriod = async (
   req: Request,
@@ -7,17 +14,28 @@ export const checkReservationPeriod = async (
   next: NextFunction,
 ) => {
   // Check if there are duplicated reservation time
-  const { startTime, endTime } = req.body
+  const { startTime, endTime, deviceId } = req.body
+  const parsedStartTime = Date.parse(startTime)
+  const parsedEndTime = Date.parse(endTime)
   try {
-    const reservation = await Reservation.findOne({ startTime })
-    if (reservation) {
-      res.status(400).json({
+    const query = {
+      deviceId,
+      startTime: { $gte: getISOString(parsedStartTime) },
+      endTime: { $lte: getISOString(parsedEndTime) },
+    }
+    const reservationList = await Reservation.find(query)
+    if (reservationList.length > 0) {
+      return res.status(400).json({
         message: `Failed! time range ${startTime} - ${endTime} is reserved`,
       })
-      next()
     }
-  } catch (err) {
-    res.status(500).json({ message: err })
-    return
+    req.body = {
+      ...req.body,
+      startTime: getISOString(parsedStartTime),
+      endTime: getISOString(parsedEndTime),
+    }
+    next()
+  } catch (error) {
+    return res.status(500).json({ message: error })
   }
 }
