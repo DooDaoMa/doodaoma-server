d3ab213bfaed4d2981cb4cb6c63f761984f6887a
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-02-04 14:50:08 +0700

    add checkUserExist middleware

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,6 +1,22 @@
 import { Request, Response, NextFunction } from 'express'
 import { User } from '../models/user'
 
+export const checkUserExist = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const user = await User.findOne({ username: req.body.username })
+    if (user) {
+      next()
+    }
+    return res.status(404).json({ message: 'User not found' })
+  } catch (error) {
+    return res.status(500).json({ message: error })
+  }
+}
+
 export const loginMiddleware = async (
   req: Request,
   res: Response,
