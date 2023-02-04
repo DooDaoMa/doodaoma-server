c2d67a801a2ca4bb8c9b7310bff4ee0046f659b9
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-02-04 14:53:18 +0700

    change response content type

--- src/routes/reservationRoute.ts
+++ src/routes/reservationRoute.ts
@@ -4,14 +4,22 @@ import {
   getArrayPages,
 } from 'express-paginate'
 import { requireJWTAuth } from '../config/jwt.config'
+import { checkDeviceExist } from '../middlewares/device.middleware'
 import { checkReservationPeriod } from '../middlewares/reservation.middleware'
 import { Reservation } from '../models/reservation'
+// import { generateDateTimeSlots } from '../utils/timeSlot'
 
 export const reservationRouter = Router()
 
 reservationRouter.use(paginateMiddleware(10, 50))
 
-reservationRouter.get('/reservations', async (req, res, next) => {
+// reservationRouter.get('/time-slot', (req, res) => {
+//   const date = new Date(new Date())
+//   date.setDate(date.getDate() + 2)
+//   res.status(200).send(generateDateTimeSlots(date))
+// })
+
+reservationRouter.get('/reservations', async (req, res) => {
   const limit = +(req.query.limit || 10)
   const skip = +(req.skip || 0)
   const page = +(req.query.skip || 1)
@@ -22,62 +30,62 @@ reservationRouter.get('/reservations', async (req, res, next) => {
     ])
     const pageCount = Math.ceil(itemCount / limit)
     if (results) {
-      res.status(200).send({
+      return res.status(200).json({
         data: results,
         pageCount,
         itemCount,
         pages: getArrayPages(req)(3, pageCount, page),
       })
     }
-  } catch (err) {
-    next(err)
+    return res.status(404).json({ message: 'There are no any reservation' })
+  } catch (error) {
+    return res.status(500).json(error)
   }
 })
 
-reservationRouter.get(
-  '/reservation',
-  requireJWTAuth,
-  async (req, res, next) => {
-    const id = +(req.body || 0)
-    try {
-      const reservation = await Reservation.findById({ _id: id })
-      if (reservation) {
-        res.status(200).send({
-          ...reservation,
-        })
-      }
-    } catch (err) {
-      next(err)
+reservationRouter.get('/reservation/:id', requireJWTAuth, async (req, res) => {
+  const _id = req.params.id
+  try {
+    const reservation = await Reservation.findById(_id).lean()
+    if (reservation) {
+      return res.status(200).json({
+        ...reservation,
+      })
     }
-  },
-)
+    return res.status(404).json({ message: `There is no document id ${_id}` })
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+})
 
 reservationRouter.post(
   '/reservation',
-  [requireJWTAuth, checkReservationPeriod],
+  [requireJWTAuth, checkDeviceExist, checkReservationPeriod],
   async (req: Request, res: Response) => {
     const payload = req.body
     try {
-      const newReservation = new Reservation({
-        username: payload.username,
-        telescopeId: payload.telescopeId,
-        createdAt: Date.now(),
-        startTime: payload.startTime,
-        endTime: payload.endTime,
-      })
+      const newReservation = new Reservation({ ...payload })
       await newReservation.save()
-      res.status(200).json(newReservation)
+      return res.status(200).json(newReservation)
     } catch (error) {
-      res.status(404).json({ message: error })
+      return res.status(404).json({ message: error })
     }
   },
 )
 
-reservationRouter.delete('/reservation', requireJWTAuth, async (req, res) => {
-  const { startTime, endTime } = req.body
-  try {
-    await Reservation.deleteOne({ startTime, endTime })
-  } catch (error) {
-    res.status(404).json({ message: error })
-  }
-})
+reservationRouter.delete(
+  '/reservation/:id',
+  requireJWTAuth,
+  async (req, res) => {
+    const _id = req.params.id
+    try {
+      const reservation = await Reservation.deleteOne({ _id })
+      if (reservation.deletedCount > 0) {
+        return res.status(200).json({ message: 'success' })
+      }
+      return res.status(404).json({ message: `There is no document id ${_id}` })
+    } catch (error) {
+      return res.status(500).json({ message: error })
+    }
+  },
+)
