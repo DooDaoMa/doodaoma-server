88eb9184c4bf85041f89117ca589bd0a312b9281
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2023-03-13 19:59:35 +0700

    impl try-catch in time slot job

--- src/services/manageTimeSlot.ts
+++ src/services/manageTimeSlot.ts
@@ -1,16 +1,23 @@
 import { schedule } from 'node-cron'
-import { startOfYesterday, endOfYesterday } from 'date-fns'
+import { startOfYesterday, endOfYesterday, addDays } from 'date-fns'
 import { TimeSlot } from '../models/timeSlot'
 import { generateDateTimeSlots } from '../utils/timeSlot'
 
-schedule('0 0 * * 0', async () => {
-  const newTimeSlot = generateDateTimeSlots(new Date())
-  console.log(
-    `add ${newTimeSlot.length} time slot from ${newTimeSlot[0]} - ${
-      newTimeSlot[newTimeSlot.length - 1]
-    } to TimeSlot collection`,
-  )
-  await TimeSlot.insertMany(newTimeSlot)
+schedule('0 0 * * 2', async () => {
+  const newTimeSlot = []
+  for (let i = 0; i < 7; i++) {
+    newTimeSlot.push(...generateDateTimeSlots(addDays(new Date(), i)))
+  }
+  try {
+    await TimeSlot.insertMany(newTimeSlot)
+    console.log(
+      `add ${newTimeSlot.length} time slot from ${newTimeSlot[0]} - ${
+        newTimeSlot[newTimeSlot.length - 1]
+      } to TimeSlot collection`,
+    )
+  } catch (error) {
+    console.error(error)
+  }
 })
 
 schedule('0 0 * * *', async () => {
@@ -18,12 +25,18 @@ schedule('0 0 * * *', async () => {
     startTime: { $gte: startOfYesterday() },
     endTime: { $lte: endOfYesterday() },
   }
-  const yesterdaySlot = await TimeSlot.find(query)
-  if (yesterdaySlot) {
-    await TimeSlot.deleteMany(yesterdaySlot)
-    console.log(
-      `remove ${yesterdaySlot[yesterdaySlot.length]} from TimeSlot collection`,
-    )
+  try {
+    const yesterdaySlot = await TimeSlot.find(query)
+    if (yesterdaySlot) {
+      await TimeSlot.deleteMany(yesterdaySlot)
+      console.log(
+        `remove ${
+          yesterdaySlot[yesterdaySlot.length]
+        } from TimeSlot collection`,
+      )
+    }
+    console.log("there're no any time slots in yesterday")
+  } catch (error) {
+    console.error(error)
   }
-  console.log("there're no any time slots in yesterday")
 })
