da07def44ff9296d5c5681015f1569fa7fbcce0f
nicenicegame <nicenicegame@gmail.com>
2023-04-17 14:17:40 +0700

    add image timestamp and image get&delete

--- src/models/image.ts
+++ src/models/image.ts
@@ -2,9 +2,12 @@ import { Schema } from 'mongoose'
 import { db } from '../config/database.config'
 import { IImage } from '../types/image.types'
 
-const ImageSchema = new Schema({
-  name: String,
-  userId: String,
-})
+const ImageSchema = new Schema(
+  {
+    name: String,
+    userId: String,
+  },
+  { timestamps: true },
+)
 
 export const Image = db.model<IImage>('Image', ImageSchema)
